7f87e39488ad6d97e78c61e182f8202cd6d7f24c
wxtan2 <wxtan2@graduate.utm.my>
2022-01-12 04:03:12 +0800

    Almost

--- resources/views/tutor/tutorClass.blade.php
+++ resources/views/tutor/tutorClass.blade.php
@@ -5,52 +5,83 @@
     @php
     $userCurrent = Auth::guard('web');
     $classes = DB::table('classes')
-        ->where('emailTutor', $userCurrent->user()->email)
+        ->leftJoin('enroll', 'classes.id', '=', 'enroll.idClass')
+        ->where('classes.emailTutor', $userCurrent->user()->email)
+        ->whereNotNull('classes.dayOfWeek')
+        ->selectRaw('classes.id, classes.className, classes.subject, classes.dayOfWeek, classes.startTime, classes.endTime, count(enroll.idClass) as numberEnroll')
+        ->groupBy('classes.id', 'classes.className', 'classes.subject', 'classes.dayOfWeek', 'classes.startTime', 'classes.endTime')
         ->get();
 
-    $getDayofWeek = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday");
+    //  $numberEnrolled = DB::table('enroll')
+    //     ->join('classes', 'enroll.idClass', '=', 'classes.id')
+    //     ->where('classes.emailTutor', $userCurrent->user()->email)
+    //     ->whereNotNull('classes.dayOfWeek')
+    //     ->slect();
 
-    function secondsToTime($seconds) {
-    $dtF = new \DateTime('@0');
-    $dtT = new \DateTime("@$seconds");
-    return $dtF->diff($dtT)->format('%h h %i min');
-}
+    $getDayofWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thusday', 'Friday', 'Saturday'];
+
+    function secondsToTime($seconds)
+    {
+        $dtF = new \DateTime('@0');
+        $dtT = new \DateTime("@$seconds");
+        return $dtF->diff($dtT)->format('%h h %i min');
+    }
     @endphp
     <div class="containerSetting">
         <div class="mainContent">
-            <table class="classTable">
-                <tr style="color:#F28F3B;">
-                    <td>No</td>
-                    <td>Class Name</td>
-                    <td>Subject</td>
-                    <td>Day</td>
-                    <td>Start Time</td>
-                    <td>End Time</td>
-                    <td>Duration</td>
-                    <td>Students</td>
-                    <td></td>
-                </tr>
-                @foreach ($classes as $classes)
-                    <tr>
-                        <td>{{ ++$loop->index }}</td>
-                        <td>{{ $classes->className }}</td>
-                        <td>{{ $classes->subject }}</td>
-                        <td>{{ $day = $getDayofWeek[$classes->dayOfWeek] }}</td>
-                        <td>{{ $classes->startTime }}</td>
-                        <td>{{ $classes->endTime }}</td>
-                        <td>{{ secondsToTime(strtotime($classes->endTime) - strtotime($classes->startTime)) }}</td>
+            @if ($classes->count() <= 0)
+                <div class="notFoundContainer"><img src="{{ URL::asset('/image/notfound.png') }}" style="width:60%;">
+                    <div style="margin-bottom:15px;">
+                        <h2 style="font-size:28px;color:#F28F3B;font-weight:600;">No Class Found</h2>
+                    </div>
+                    <div><a class="notfoundLink" href="{{ route('dashboard') }}" style="color:#808080;">Add Class in Your
+                            Timetable</a></div>
+                </div>
+            @else
+                <table class="classTable">
+                    <tr style="color:#F28F3B;">
+                        <td>No</td>
+                        <td>Class Name</td>
+                        <td>Subject</td>
+                        <td>Day</td>
+                        <td>Start Time</td>
+                        <td>End Time</td>
+                        <td>Duration</td>
+                        <td style="text-align: center;">Students</td>
                         <td></td>
-                        <td>
-                            <input type="hidden" value="{{$classes->id}}">
-                            <input type="submit" class="viewButton" value="">
-                        </td>
                     </tr>
-                @endforeach
-            </table>
+                    @foreach ($classes as $classes)
+                        <tr>
+                            <td>{{ ++$loop->index }}</td>
+                            <td>{{ $classes->className }}</td>
+                            <td>{{ $classes->subject }}</td>
+                            <td>{{ $day = $getDayofWeek[$classes->dayOfWeek] }}</td>
+                            <td>{{ $classes->startTime }}</td>
+                            <td>{{ $classes->endTime }}</td>
+                            <td>{{ secondsToTime(strtotime($classes->endTime) - strtotime($classes->startTime)) }}</td>
+                            <td class="numberEnroll">{{ $classes->numberEnroll }}</td>
+                            <td>
+                                <form action="/classes/details" method="get">
+                                    @csrf
+                                    <input type="hidden" name="id" value="{{ $classes->id }}">
+                                    <button type="submit" class="viewButton" value=""></button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            @endif
         </div>
     </div>
 
     <style>
+        .numberEnroll {
+            font-size: 32px;
+            font-weight: 600;
+            text-align: center;
+            color: #F28F3B;
+        }
+
         .containerSetting {
             width: calc(100% - 13.54vw);
             display: flex;
@@ -61,6 +92,19 @@
             box-sizing: border-box;
         }
 
+        .notFoundContainer {
+            text-align: center;
+        }
+
+        .notfoundLink {
+            color: #808080;
+            text-decoration: none;
+        }
+
+        .notfoundLink:hover {
+            text-decoration: underline;
+        }
+
         .mainContent {
             width: 100%;
             display: flex;
@@ -68,6 +112,7 @@
             height: 90vh;
             padding: 0 0 0 10px;
             box-sizing: border-box;
+            justify-content: center;
         }
 
         .classTable {
@@ -84,7 +129,7 @@
         }
 
         .classTable tr:nth-child(n+2) {
-            
+
             border-radius: 10px;
             box-shadow: 0px 4px 35px rgba(154, 161, 171, 0.15);
             background: #ffffff;
@@ -113,13 +158,24 @@
 
         .viewButton {
             width: 45px;
-            padding: 8px;
-            background-image: url('/image/View.png');
+            height: 45px;
+            display: flex;
+            background-image: url('{{ URL::asset('/image/eye 1.svg') }}');
             background-position: center;
-            background-size: 40px;
+            background-size: 30px;
             background-repeat: no-repeat;
             border-radius: 10px;
             vertical-align: middle;
+            outline: 0px;
+            border: 1px solid #aaaaaa;
+            cursor: pointer;
+        }
+
+        .viewButton:hover {
+            border: 1px solid #F28F3B;
+            background-color: #ffe5ce;
+            cursor: pointer;
+            background-image: url('{{ URL::asset('/image/eye 1 copy.svg') }}');
         }
 
     </style>
