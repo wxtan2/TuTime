dba53f316339f03a4ae3cdf8f6bf1c2d563785ec
wxtan2 <wxtan2@graduate.utm.my>
2022-01-13 01:36:29 +0800

    student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\Student as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
